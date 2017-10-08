505314edb981048646db181a27e2d99e0fe6cc14
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2017-10-08 21:12:33 +0200

    Added BaseCommand

--- src/Command.ts
+++ src/Command.ts
@@ -5,3 +5,9 @@ export interface Command {
 export function createCommand(commandName: string, extraProperties?: object): Command {
     return Object.freeze({...extraProperties, command: commandName});
 }
+
+export class BaseCommand implements Command {
+    constructor(public readonly command: string, data: { [key: string]: any }) {
+        Object.freeze(this);
+    }
+}
