edc48d89ff8dd6994a3aca7d133caa179d8e6622
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2017-10-16 23:54:49 +0200

    Added createCommandFactory

--- src/Command.ts
+++ src/Command.ts
@@ -2,13 +2,43 @@ export interface Command {
     command: string;
 }
 
+/**
+ * Simple helper to create immutable commands
+ */
 export function createCommand<T extends object = object>(commandName: string, extraProperties?: T): Readonly<Command & T> {
     return Object.freeze(Object.assign({}, extraProperties, {command: commandName}));
 }
 
+/**
+ * Very basic helper class if you want to create Commands using classes.
+ * Simply extend it and provide proper arguments in constructor.
+ */
 export class BaseCommand implements Command {
     constructor(public readonly command: string, extraProperties?: { [key: string]: any }) {
         Object.assign(this, {command}, extraProperties);
         Object.freeze(this);
     }
+}
+
+export interface CommandFactory<T> {
+    (input: T): Readonly<Command & T>;
+
+    command: string;
+}
+
+/**
+ * Helper method to create command factory.
+ *
+ * Returned factory is a wrapper to "createCommand" helper and passes all provided arguments
+ *
+ * @param name command name
+ * @param onCreate function called on command creation
+ */
+export function createCommandFactory<T extends object>(name: string, onCreate?: (input: T, name: string) => any): CommandFactory<T> {
+    const func = <CommandFactory<T>>function (input: T) {
+        onCreate && onCreate(input, name);
+        return createCommand(name, input);
+    };
+    func.command = name;
+    return func;
 }
--- test/CommandTest.ts
+++ test/CommandTest.ts
@@ -1,5 +1,6 @@
-import {BaseCommand, createCommand} from "../src/Command";
+import {BaseCommand, createCommand, createCommandFactory} from "../src/Command";
 import {assert} from 'chai';
+import * as sinon from 'sinon';
 
 describe('Command', () => {
     const NAME = 'commandName';
@@ -25,6 +26,32 @@ describe('Command', () => {
         });
 
         assert.isTrue(Object.isFrozen(command));
+    });
+
+    it('command factory', () => {
+        const commandFactory = createCommandFactory<{ prop: string }>(NAME);
+
+        assert.strictEqual(commandFactory.command, NAME);
+
+        assert.deepEqual(commandFactory({prop: 'test'}), {
+            command: NAME,
+            prop: 'test'
+        });
+    });
+
+    it('command factory with onCreate callback', () => {
+        const onCreate = sinon.spy();
+        const commandFactory = createCommandFactory(NAME, onCreate);
+
+        const input = {some: 'input', object: 'with data'};
+
+        assert.deepEqual(commandFactory(input), {command: NAME, ...input});
+
+        sinon.assert.calledWithExactly(
+            onCreate,
+            input,
+            NAME
+        );
     });
 });
 
