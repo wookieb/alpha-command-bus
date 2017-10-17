e9eba84fb47a08d2e9c183285902a9f9787d6d02
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2017-10-17 23:34:14 +0200

    Remove old test code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
 export * from './Command';
 export * from './CommandBus';
 export * from './decorators';
-
-
-export class Class1 {
-    constructor() {
-        Object.freeze(this);
-    }
-}
-
-export class Class2
